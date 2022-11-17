e6786eb9f15864c9f4ad78ac8df0c2aca2da6f71
SerggSmirnov <lam97@yandex.ru>
2022-11-17 21:27:04 +0300

    fix api key

--- FitnessApp/NetworkRequest/NetworkRequest.swift
+++ FitnessApp/NetworkRequest/NetworkRequest.swift
@@ -12,8 +12,6 @@ class NetworkRequest {
     static let shared = NetworkRequest()
     private init() {}
     
-//  https://api.openweathermap.org/data/2.5/weather?&appid=b5d83b5c0289595b2e8a0170e5f2eef0&units=metric&lat=55.96&lon=38.04
-    
     func requestData(completion: @escaping (Result<Data, Error>) -> Void) {
         let key = "b5d83b5c0289595b2e8a0170e5f2eef0"
         let latitude = 55.96
